48de90314e746920de58639268cec634e34f0f4e
evgen-kaz <itroute@mail.ru>
2025-09-30 14:39:33 +0800

    added project

---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,35 @@
+package tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.TextBoxPage;
+
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+import static pages.TextBoxPage.*;
+
+@Tag("Registration")
+public class TextBoxTests extends TestBase {
+    TextBoxPage textBoxPage = new TextBoxPage();
+
+    @Test
+    void fillFormTest() {
+        textBoxPage.openPage();
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("window.onloadCallback = function() { console.log('Fake reCAPTCHA onloadCallback called'); };");
+        textBoxPage.setFirstName(firstName)
+                .setEmail(email)
+                .setCurrentAddress(currentAddress)
+                .setPermanentAddress(permanentAddress);
+        textBoxPage.openPage();
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("window.onloadCallback = function() { console.log('Fake reCAPTCHA onloadCallback called'); };");
+        textBoxPage.clickButton()
+                .checkResult("Name:", firstName)
+                .checkResult("Email:", email)
+                .checkResult("Current Address :", currentAddress)
+                .checkResult("Permananet Address :", permanentAddress);
+    }
+}
+
